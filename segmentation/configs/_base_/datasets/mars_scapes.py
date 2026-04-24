3053e760bc1f7ebbfaf9467f38d4964e37a0c9b0
HelloWorld <1297909082@qq.com>
2026-04-24 15:22:14 +0800

    add mars datasets

---
+++ segmentation/configs/_base_/datasets/mars_scapes.py
@@ -0,0 +1,80 @@
+# dataset settings
+dataset_type = 'MarsscapesDataset'
+data_root = 'data/mars-scapes'
+
+crop_size=(256,512)
+
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations'),
+    dict(type='RandomResize', scale=(1024,512), ratio_range=(0.5, 2.0)),
+    dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PhotoMetricDistortion'),
+    dict(type='PackSegInputs')
+]
+
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='Resize', scale=(512,256),keep_ratio=True),
+    dict(type='LoadAnnotations'),
+    dict(type='PackSegInputs')
+]
+
+'''
+img_ratios = [0.5, 0.75, 1.0, 1.25, 1.5, 1.75]
+tta_pipeline = [
+    dict(type='LoadImageFromFile', backend_args=None),
+    dict(
+        type='TestTimeAug',
+        transforms=[
+            [
+                dict(type='Resize', scale_factor=r, keep_ratio=True)
+                for r in img_ratios
+            ],
+            [
+                dict(type='RandomFlip', prob=0., direction='horizontal'),
+                dict(type='RandomFlip', prob=1., direction='horizontal')
+            ], [dict(type='LoadAnnotations')], [dict(type='PackSegInputs')]
+        ])
+]
+'''
+
+train_dataloader = dict(
+    batch_size=4,
+    num_workers=2,
+    persistent_workers=True,
+    sampler=dict(type='InfiniteSampler', shuffle=True),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        data_prefix=dict(
+            img_path='img/train', seg_map_path='label_id/train'),
+        pipeline=train_pipeline))
+
+val_dataloader = dict(
+    batch_size=1,
+    num_workers=4,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        data_prefix=dict(
+            img_path='img/val', seg_map_path='label_id/val'),
+        pipeline=test_pipeline))
+
+test_dataloader = dict(
+    batch_size=1,
+    num_workers=4,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        data_prefix=dict(
+            img_path='img/test', seg_map_path='label_id/test'),
+        pipeline=test_pipeline))
+
+val_evaluator = dict(type='IoUMetric', iou_metrics=['mIoU'])
+test_evaluator = val_evaluator
---
+++ segmentation/configs/vssd/vssd_small_mar-seg.py
@@ -0,0 +1,32 @@
+
+_base_ = [
+    '../swin/swin-tiny_mars-seg.py'
+]
+
+
+model = dict(
+    backbone=dict(
+        type='MM_VSSD',
+        out_indices=(0, 1, 2, 3),
+        pretrained="change to the path of the pretrained model",
+        embed_dim=64,
+        depths=(3, 4, 21, 5),
+        num_heads=(2, 4, 8, 16),
+        simple_downsample=False,
+        simple_patch_embed=False,
+        ssd_expand=2,
+        ssd_chunk_size=256,
+        linear_attn_duality=True,
+        attn_types=['mamba2', 'mamba2', 'mamba2', 'standard'],
+        bidirection=False,
+        drop_path_rate=0.4,
+        d_state=64,
+        ssd_positve_dA=True,
+        key='model_ema'
+    ),
+    decode_head=dict(in_channels=[64, 128, 256, 512], num_classes=9,act_cfg=dict(type='ReLU', inplace=False)),
+    auxiliary_head=dict(in_channels=256, num_classes=9,act_cfg=dict(type='ReLU', inplace=False),)
+)
+
+# train_dataloader = dict(batch_size=4) # as gpus=4
+
